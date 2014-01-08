e6b864903b312cfd779b9078430fd51b4fe8493f
Darcey Lachtman <dslachtman@gmail.com>
2014-01-08 11:30:03 -0800

    almost done release 1

--- source/factorial.rb
+++ source/factorial.rb
@@ -7,6 +7,14 @@ end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
+	if n == 0
+		puts n 
+	end
+	fact = n * (n -1)
+	puts n 
+	factorial_recursive(n)
+	puts fact 
+
 end
 
 ###driver code
@@ -14,3 +22,7 @@ factorial_iterative(5)
 factorial_iterative(0)
 factorial_iterative(3)
 
+
+factorial_recursive(5)
+factorial_recursive(6)
+factorial_recursive(1)
