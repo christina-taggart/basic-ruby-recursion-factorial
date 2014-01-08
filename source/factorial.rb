6c7213486b79de81bad56af1b68aebe7e6fa1662
Apprentice <apprentice@dbc28.local>
2014-01-08 11:01:54 -0800

    Release 0: first working version of iterative solution to factorial

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,28 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+  if n==0
+    1
+  else
+    range = (1..n)
+    range.reduce(:*)
+  end
 end
+
+p factorial_iterative(0) == 1
+p factorial_iterative(1) == 1
+p factorial_iterative(4) == 24
+p factorial_iterative(11) == 39916800
+p factorial_iterative(5) != 500
+p factorial_iterative(3) != 472
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
+
 end
+
+p factorial_recursive(0) == 1
+p factorial_recursive(1) == 1
+p factorial_recursive(4) == 24
+p factorial_recursive(11) == 39916800
+p factorial_recursive(5) != 500
+p factorial_recursive(3) != 472
