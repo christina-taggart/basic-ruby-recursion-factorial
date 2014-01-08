ba177f8d0729a5016dbdb3c6b6072454642087b8
Apprentice <apprentice@dbc04.local>
2014-01-08 11:01:54 -0800

    Add iterative and recursive functions for factorial

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,23 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+  result = 1
+  while n > 0
+    result *= n
+    n-=1
+  end
+  result
 end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
+  return 1 if n == 0
+  n * factorial_recursive(n-1)
 end
+
+p factorial_iterative(5) == 120
+p factorial_iterative(1) == 1
+p factorial_iterative(0) == 1
+
+p factorial_recursive(5) == 120
+p factorial_recursive(1) == 1
+p factorial_recursive(0) == 1
