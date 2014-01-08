ba0615e5e36b109786ce0c979802ecd58c8c069b
Darcey Lachtman <dslachtman@gmail.com>
2014-01-08 11:10:11 -0800

    release 0, solved iterative factorial

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,16 @@
 # Implement an iterative version of the factorial function
+
 def factorial_iterative(n)
+	fact = (1..n).inject(:*)
+	puts fact
 end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
 end
+
+###driver code
+factorial_iterative(5)
+factorial_iterative(0)
+factorial_iterative(3)
+
