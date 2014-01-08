e1f99e5ae905f90d75998a81c95fadcf2cc2e458
Apprentice <apprentice@dbc03.local>
2014-01-08 10:52:35 -0800

    Adds iterative solution for release 0

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,27 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+  return 1 if n == 0
+  solution = 1
+  n.times {
+    solution *= n
+    n -= 1
+  }
+  solution
 end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
 end
+
+#-----drivers-----
+puts "iterative:"
+p factorial_iterative(0) == 1
+p factorial_iterative(3) == 6
+p factorial_iterative(5) == 120
+p factorial_iterative(10) == 3628800
+
+puts "recursive:"
+p factorial_recursive(0) == 1
+p factorial_recursive(3) == 6
+p factorial_recursive(5) == 120
+p factorial_recursive(10) == 3628800
