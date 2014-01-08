c9811943f1ddecab009156e0849ad89cfa622fb9
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-01-08 12:06:19 -0800

    Release 1: Implements recursive factorial function

--- source/factorial.rb
+++ source/factorial.rb
@@ -5,7 +5,8 @@ end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
+	n > 1 ? n * factorial_recursive(n-1) : n
 end
 
-
 p factorial_iterative(5) == 120
+p factorial_recursive(5) == 120
