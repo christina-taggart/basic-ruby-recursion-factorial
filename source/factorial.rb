2b3eecdace9fda7c0743091c52d378ddff7d120a
Apprentice <apprentice@dbc05.local>
2014-01-08 12:24:16 -0800

    Release 0 and Release 1

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,31 @@
-# Implement an iterative version of the factorial function
-def factorial_iterative(n)
+# # Implement an iterative version of the factorial function
+def factorial_iterative(num)
+  if num > 0
+    factorial = num * (num - 1)
+    until num == 2
+      num = (num - 1)
+      factorial = factorial * (num - 1)
+    end
+    return factorial
+  elsif num < 0
+    puts "error"
+  elsif num == 0
+    puts 1
+  end
 end
 
+p factorial_iterative(7) == 5040
+p factorial_iterative(4) == 24
+p factorial_iterative(10) == 3628800
 # Implement a recursive version of the factorial function
-def factorial_recursive(n)
+def factorial_recursive(num)
+  if num == 1
+    return 1
+  else
+    num * factorial_recursive(num-1)
+  end
 end
+
+p factorial_recursive(7) == 5040
+p factorial_recursive(4) == 24
+p factorial_recursive(10) == 3628800
