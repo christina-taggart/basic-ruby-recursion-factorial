77369a72b9030d37ec202a134e91d13da12575ff
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-01-08 11:54:27 -0800

    Release 0: Implements iterative factorial function

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,7 +1,11 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+	(2..n).inject(:*)
 end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
 end
+
+
+p factorial_iterative(5) == 120
