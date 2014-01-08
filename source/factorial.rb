42461c0d357f60bc7b2320dbf1c910e71ca2eb87
Apprentice <apprentice@dbc08.local>
2014-01-08 11:17:08 -0800

    Release 0

--- source/factorial.rb
+++ source/factorial.rb
@@ -1,5 +1,8 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+  return 1 if n.zero?
+  1.upto(n).inject {|product, x| product * x}
+  #1.upto(n).inject(:*)
 end
 
 # Implement a recursive version of the factorial function
